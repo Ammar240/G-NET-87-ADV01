5da640f80044df55e75a67dfeb60cfa6e333a42c
Ammar <ammar@gmil.com>
2026-08-30 13:27:01 +0300

    solve question 2

---
+++ Container.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Advanced_S01_Generics;
+
+internal class Container<T>
+{
+    private T tItem;
+    public void Add(T item)
+    {
+        this.tItem = item;
+    }
+    public T Get() { return this.tItem; }
+}
