fe3a7466a6a041455b9a6f1bce0130775fe446bd
Ammar <ammar@gmil.com>
2026-08-30 14:20:57 +0300

    solve question 12

--- BacseConstraint.cs
+++ BacseConstraint.cs
@@ -14,10 +14,15 @@ internal class Child : Base
 {
 }
 
-internal class GenClass<T> where T : Base
+internal class GenClass<T> where T : Base , IRepository<T>, new()
 {
    // T could be of type base or any of its derived types 
    // base or child
+
+   // To apply multiple constarints 
+   // 1. class or struct or base class  (only one of the three)
+   // 2. interface constraints (any number of intferfaces)
+   // 3. new() constraint
 }
 
 
