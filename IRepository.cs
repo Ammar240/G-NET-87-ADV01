7d222e0d92d4cad40b65edc4af5d079975c9caf7
Ammar <ammar@gmil.com>
2026-08-30 14:01:46 +0300

    solve question 6

---
+++ IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Advanced_S01_Generics;
+
+internal interface IRepository<T>
+{
+    // Generic interface -> interface with any type T (place holder)
+
+}
