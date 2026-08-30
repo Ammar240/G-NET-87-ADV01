a817ad088f64fd9a9eb16a62dc1d517dc8110f7e
Ammar <ammar@gmil.com>
2026-08-30 14:18:00 +0300

    solve question 11

---
+++ BacseConstraint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Advanced_S01_Generics;
+
+internal class Base
+{
+}
+
+internal class Child : Base
+{
+}
+
+internal class GenClass<T> where T : Base
+{
+   // T could be of type base or any of its derived types 
+   // base or child
+}
+
+
+
