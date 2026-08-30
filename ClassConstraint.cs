bb75d71bfca3061eb85b5432349bc6b6f7f8603f
Ammar <ammar@gmil.com>
2026-08-30 14:09:51 +0300

    solve question 8

---
+++ ClassConstraint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Advanced_S01_Generics;
+
+internal class ClassConstraint<T> where T : class
+{
+    // T is place holder for class types (rference types)  ( string ,array ...)
+    // value types are not allowed
+    // can be null
+}
