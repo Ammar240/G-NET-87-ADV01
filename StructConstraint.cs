4f5fb509314d75ecd9ac76d0bb2d9d6cdce36f51
Ammar <ammar@gmil.com>
2026-08-30 14:06:45 +0300

    solve question 7

---
+++ StructConstraint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Advanced_S01_Generics;
+
+internal class StructConstraint<T> where T : struct
+{
+    // T is place holder for struct(value) types only ( int, double..)
+    // class types are not allowed
+}
